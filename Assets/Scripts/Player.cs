4fb4f2279d83c0800323b45d5a96e90d98fa3613
rlathdgml0405 <lokijuhyg09@gmail.com>
2024-12-07 01:21:27 +0900

    1207 사슴 움직이기 / 드래그로 화면 전환 / 맵 수정 / 물 충돌 시 시작지점으로 리셋

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,115 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed;
+    public float rotSpeed;
+    float hAxis;
+    float vAxis;
+    float MouseX;
+    bool wDown;
+    bool jDown;
+
+    bool isJump;
+
+    Vector3 moveVec;
+    Rigidbody rigid;
+    Animator anim;
+    private Vector3 startPosition; 
+
+    void Awake()
+    {
+        anim = GetComponentInChildren<Animator>();
+        rigid = GetComponent<Rigidbody>();
+        startPosition = transform.position;
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetInput();
+        Move();
+        Turn();
+        Jump();
+
+
+    }
+
+    void FreezeRotation()
+    {
+        rigid.angularVelocity = Vector3.zero;
+    }
+
+    private void FixedUpdate()
+    {
+        FreezeRotation();
+    }
+
+    void GetInput()
+    {
+        hAxis = Input.GetAxisRaw("Horizontal");
+        vAxis = Input.GetAxisRaw("Vertical");
+        wDown = Input.GetButton("Walk");
+        jDown = Input.GetButton("Jump");
+        MouseX = Input.GetAxis("Mouse X");   
+    }
+
+    void Move()
+    {
+        // moveVec = new Vector3(hAxis, 0, vAxis).normalized;
+
+        //transform.position += moveVec * speed * (wDown ? 0.3f : 1f) * Time.deltaTime;
+
+        Vector3 hori = transform.right * hAxis;
+        Vector3 vert = transform.forward * vAxis;
+
+        moveVec=(hori+vert).normalized*speed;
+        rigid.MovePosition(transform.position+moveVec*Time.deltaTime);
+
+
+
+        anim.SetBool("isRun", moveVec != Vector3.zero);
+        anim.SetBool("iswalk", wDown);
+    }
+
+    void Turn()
+    {
+        //transform.LookAt(transform.position + moveVec);
+        //transform.Rotate(Vector3.up * hAxis * speed * Time.deltaTime);
+       if (Input.GetMouseButton(0))
+        {
+            transform.Rotate(Vector3.up * rotSpeed * MouseX);
+        }
+          
+        
+    }
+
+    void Jump()
+    {
+        if (jDown && !isJump)
+        {
+            rigid.AddForce(Vector3.up * 2000, ForceMode.Impulse);
+            anim.SetBool("isJimp", true);
+            anim.SetTrigger("doJump"); 
+            isJump = true;
+        }
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Floor")
+        {
+            anim.SetBool("isJump", false);
+            isJump = false;
+        }
+
+        if(collision.gameObject.tag == "Water")
+        {
+            transform.position = startPosition;
+        }
+    }
+}
